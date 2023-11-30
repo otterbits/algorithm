17dabcf292567476ff1cbd23ba6dd95687d37887
otter <144116866+otterbits@users.noreply.github.com>
2023-11-30 22:38:11 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,14 @@
+import math
+
+def solution(price):
+    answer = 0
+    if price < 100000:
+        price == price
+    if 300000 > price >= 100000:
+        price -= price * 0.05
+    elif 500000 > price >= 300000:
+        price -= price * 0.1
+    elif price >= 500000:
+        price -= price * 0.2
+    answer = math.trunc(price)
+    return answer
