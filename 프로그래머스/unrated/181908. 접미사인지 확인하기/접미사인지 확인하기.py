618f48dcedfe8562926a85b5c26e98780530c021
otter <144116866+otterbits@users.noreply.github.com>
2023-12-16 16:15:28 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_suffix):
+    answer = 0
+    for i in range(len(my_string)):
+        if my_string[i:] == is_suffix:
+            return 1
+        else:
+            answer = 0
+    return answer
