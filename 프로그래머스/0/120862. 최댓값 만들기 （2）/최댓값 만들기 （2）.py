da8408e974e2f3ff6cfbe2178fd4a146e9a4f9fd
otter <144116866+otterbits@users.noreply.github.com>
2024-01-17 17:23:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,10 +1,3 @@
 def solution(numbers):
     asending = sorted(numbers)
-    front = asending[-2:]
-    front_answer = back_answer = 1
-    back = asending[:2]
-    for i in front:
-        front_answer *= i
-    for j in back:
-        back_answer *= j
-    return max(front_answer, back_answer)
+    return max(asending[0] * asending[1], asending[-1] * asending[-2])
