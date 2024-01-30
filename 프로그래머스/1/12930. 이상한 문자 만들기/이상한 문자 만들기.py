a8bf67d0c9a290a1ca5dbed623f502ce0919b9e1
otter <144116866+otterbits@users.noreply.github.com>
2024-01-30 15:06:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,12 @@
 def solution(s):
-    answer = []
+    answer = ''
     words = s.split(" ")
     for word in words:
-        part = []
+        part = ''
         for i in range(len(word)):
             if i % 2 == 0:
-                part.append(word[i].upper())
+                part += word[i].upper()
             else:
-                part.append(word[i].lower())
-        part = "".join(part)
-        answer.append(part)
-    answer=" ".join(answer)
-    return answer
+                part += word[i].lower()
+        answer += (part + ' ')
+    return answer[0:-1]
