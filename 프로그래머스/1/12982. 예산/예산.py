7445779a92dc25c2552bb29a2ceacf742635facc
otter <144116866+otterbits@users.noreply.github.com>
2024-01-30 13:29:07 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    answer = 0
+    d = sorted(d)
+    for i in d:
+        budget -= i
+        if budget >= 0:
+            answer += 1
+        else:
+            continue
+    return answer
