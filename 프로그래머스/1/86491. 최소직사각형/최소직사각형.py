f6afc1f90d4ce516f51a42b11f31d32557958cb1
otter <144116866+otterbits@users.noreply.github.com>
2024-01-31 16:25:34 +0900

    [level 1] Title: 최소직사각형, Time: 4.29 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,8 @@
+def solution(sizes):
+    max_width = max_height = 0
+    
+    for size in sizes:
+        width, height = size
+        max_width = max(max_width, width, height)
+        max_height = max(max_height, min(width, height))
+    return max_width * max_height
