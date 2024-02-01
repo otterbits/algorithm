ecac4d653c0f65615b6db20fe6d3f7af163ad225
otter <144116866+otterbits@users.noreply.github.com>
2024-02-01 13:49:05 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.02 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -9,7 +9,3 @@ def solution(s):
             answer.append(i - index[s[i]])
             index[s[i]] = i
     return answer
-
-            # s를 한 글자씩 돌면서 index도 같이 검색
-            # 만약, 해당 글자가 처음 나타난 거면 -1을 append,
-            # 두 번 이상 나타난 거면 해당 글자 왼쪽 첫 번째 글자 index를 뺀 값을 append,
