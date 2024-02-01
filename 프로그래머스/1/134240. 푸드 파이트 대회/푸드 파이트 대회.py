d4754f79f9ef6c8220655336f4f414a5d298c76c
otter <144116866+otterbits@users.noreply.github.com>
2024-02-01 14:24:16 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,6 +2,5 @@ def solution(food):
     answer = ''
     for i in range(1, len(food)):
         answer += str(i) * (int(food[i]) // 2)
-    answer += '0'
-    answer += answer[len(answer)-2::-1]
-    return answer
+    rev = answer[::-1]
+    return answer + '0' + rev
