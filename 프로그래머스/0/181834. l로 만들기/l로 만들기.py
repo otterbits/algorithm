9329f5ce60d689a76561b0ba9830de0a65eb09be
otter <144116866+otterbits@users.noreply.github.com>
2024-02-29 16:25:39 +0900

    [level 0] Title: l로 만들기, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,4 +1,2 @@
 def solution(myString):
-    myString = myString.replace("a","l").replace("b","l").replace("c","l").replace("d","l").replace("e","l").replace("f","l").replace("g","l").replace("h","l").replace("i","l").replace("j","l").replace("k", "l")
-    return myString
-
+    return myString.translate(str.maketrans('abcdefghijk', 'lllllllllll'))
