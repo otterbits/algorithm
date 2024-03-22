3602c8183ab5de4055a784643665c1a140923301
otter <144116866+otterbits@users.noreply.github.com>
2024-03-22 20:20:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 680 ms, Memory: 65036 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,17 +1,49 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(graph, i, visited)
+#
+#
+# n, m = map(int, input().split())
+# graph = [[] for _ in range(n + 1)]
+#
+# for _ in range(m):
+#     u, v = map(int, input().split())
+#     graph[u].append(v)
+#     graph[v].append(u)
+#
+# visited = [False] * (n + 1)
+# count = 0
+#
+# for i in range(1, n + 1):
+#     if not visited[i]:
+#         dfs(graph, i, visited)
+#         count += 1
+#
+# print(count)
+#
+
+
+def bfs(graph, start, visited):
+    visited[start] = True
+    queue = deque([start])
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
 
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-
 for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
@@ -20,9 +52,9 @@ for _ in range(m):
 visited = [False] * (n + 1)
 count = 0
 
-for i in range(1, n + 1):
+for i in range(1, n+1):
     if not visited[i]:
-        dfs(graph, i, visited)
+        bfs(graph, i, visited)
         count += 1
-
+        
 print(count)
