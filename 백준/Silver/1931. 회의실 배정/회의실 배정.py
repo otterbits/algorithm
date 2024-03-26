6dde2d6d3bcbc3f1a769e16904e600bc17a51110
otter <144116866+otterbits@users.noreply.github.com>
2024-03-26 13:25:55 +0900

    [Silver I] Title: 회의실 배정, Time: 260 ms, Memory: 59068 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -3,16 +3,16 @@ input = sys.stdin.readline
 
 N = int(input())
 graph = [list(map(int, input().split())) for i in range(N)]
-# 끝나는 시각 기준 오름차순 정렬
+# 끝나는 시각 기준 오름차순 정렬 (일찍 끝나야 다음 회의를 더 많이 진행할 수 있음)
 graph.sort(key=lambda x: (x[1], x[0]))
 std = 0
 cnt = 0
 
 for start, end in graph:
-    # 지금 시간보다 시작 시간이 클 경우
+    # 지금 종료 시간보다 다음 시작 시간이 클 경우
     if start >= std:
         cnt += 1
-        # 기준점을 끝나는 시간으로 변경
+        # 지금 종료 시간을 다음 종료 시간으로 변경
         std = end
 
 print(cnt)
