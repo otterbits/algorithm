33c6fe1bf0ac336c168e54d00cce8cd06d020406
otter <144116866+otterbits@users.noreply.github.com>
2024-05-23 15:16:55 +0900

    [Silver II] Title: 유기농 배추, Time: 236 ms, Memory: 12980 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -5,6 +5,7 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 // 테스트 케이스 개수
 const tc = input.shift();
 
+// 상하좌우 한번 써보기
 const directions = [
     [-1, 0],
     [1, 0],
@@ -12,6 +13,7 @@ const directions = [
     [0, 1]
 ]
 
+// dfs지만, test case가 여러 개기 때문에, 각각 row/col/field/visited를 받아야한다.
 function dfs (x, y, row, col, field, visited) {
     let stack = [[x, y]]
     visited[x][y] = true;
@@ -32,27 +34,35 @@ function dfs (x, y, row, col, field, visited) {
     }
 }
 
+// test case 개수만큼 탐색
 for (let i = 0; i < tc; i++) {
 
+    // 첫째 줄 가로길이, 세로길이, 배추가 심어져 있는 위치의 개수
     let [row, col, locCnt] = input.shift().split(" ");
+    // 배추가 심어져 있는 위치의 개수만큼 splice하여 'location'이라는 배추가 심어져 있는 위치 배열 생성
     let location = input.splice(0, locCnt);
+    
+    // 기본 밭의 정보(어디 심어져있는지, 밭의 2차원 배열, 방문 여부 배열) 갱신
     let field = Array.from({length: col}, () => Array.from({length: row}, () => 0));
-
     for (loc of location) {
         let [locY, locX] = loc.split(" ").map(Number);
         field[locX][locY] = 1;
     }
     let visited = Array.from({length: col}, () => Array.from({length: row}, () => false));
 
+    // 지렁이 개수
     let cnt = 0;
 
+    // 배추가 심어져 있는 위치 돌기
     for (loc of location) {
         let [locY, locX] = loc.split(" ").map(Number);
         if (!visited[locX][locY]) {
             dfs(locX, locY, row, col, field, visited);
+            // dfs하면 지렁이 개수 추가
             cnt ++;
         }
     }
 
+    // test case 한 번 끝날때 마다 지렁이 개수 출력
     console.log(cnt);
 }
